bcf00adc1d199b3b18af1e5ba6e11b0c31638d0e
Sambit Mishra <labsquantumleap@gmail.com>
2025-01-10 08:42:39 +0530

    Update streamlit_app.py
    Signed-off-by: Sambit Mishra <labsquantumleap@gmail.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -385,8 +385,9 @@ def get_prediction(age, sex, bmi, children, smoker, region, model, feature_names
 
     return round(predicted_expenses[0], 2)
 
-# Function to display the prediction page
 def show_predict_page():
+    st.markdown(f'''<h1 style="color:black;font-size:35px; text-align:center;">{"Welcome To Insurance Premium Predictor"}</h1>''', unsafe_allow_html=True)
+
     # Load and train the model, unpacking the model and feature names
     model, feature_names = load_and_train_model()
 
